0f921d7a3042ab4db7b9e7053270e20501b37aa4
Jerrel Sogoni <jerrel.sogoni@gmail.com>
2019-10-28 00:17:02 -0400

    Added: boilerplate code for other components
    Signed-off-by: JerrelSogoni <jerrel.sogoni@stonybrook.edu>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import NavBar from "./app/common/NavBar";
-import "./App.css";
+import Landing from "./app/components/Landing";
+import AboutMe from "./app/components/AboutMe";
+import Projects from "./app/components/Projects";
+import ContactMe from "./app/components/ContactMe";
+import Footer from "./app/common/Footer";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Landing />
+      <AboutMe />
+      <Projects />
+      <ContactMe />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/app/components/Landing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+  return <h1>landing</h1>;
+};
+
+export default Landing;
