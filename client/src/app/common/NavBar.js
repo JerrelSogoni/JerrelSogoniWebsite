265620a7c437c7c5a96ddb7a37fd27a5009226e3
Jerrel Sogoni <jerrel.sogoni@gmail.com>
2019-10-27 22:17:02 -0400

    Added: Simple header
    Signed-off-by: JerrelSogoni <jerrel.sogoni@stonybrook.edu>

---
+++ client/src/app/common/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const NavBar = () => {
+  return (
+    <nav className="grey lighten-3">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo center grey-text text-darken-3">
+          Jerrel Sogoni
+        </a>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
+
+//TODO: Make my name wrap correctly when displayed in resolution like an iphone 5
