e7d9c3ce8c4a7ac7ee1e635c94177a9907f5b3bf
Jerrel Sogoni <jerrel.sogoni@gmail.com>
2019-10-27 22:15:19 -0400

    Added: Simple header
    Signed-off-by: JerrelSogoni <jerrel.sogoni@stonybrook.edu>

---
+++ client/src/app/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Parallax, Background } from "react-parallax";
+
+const Landing = () => {
+  return (
+    <>
+      <h1>apples</h1>
+    </>
+  );
+};
+
+export default Landing;
